87477fa816d2488d1c65f40b6ff14a3906ebf02d
maxim dashkevich <maximkacr7@mail.com>
2020-03-15 17:48:50 +0300

    feat: complete carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const LN2 = 0.693;
+let ans;
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity <= 0 || sampleActivity > MODERN_ACTIVITY ||  /[A-Za-z]/.test(sampleActivity)) {
+return false;
+  }
+ return  Math.ceil( (Math.log( MODERN_ACTIVITY / parseFloat(sampleActivity)) /  (LN2 / HALF_LIFE_PERIOD )));
+}
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+
