f5f818cd985320b9780dd0e930266783208ef0cd
maxim dashkevich <maximkacr7@mail.com>
2020-03-19 18:36:47 +0300

    feat: complete dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,6 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let ans = members.filter(name => typeof name == 'string').map( elem => elem = elem.trim().slice(0, 1).toUpperCase() ).join('');
+    return ans.split('').sort().join('');
+  } else return false;
 };
