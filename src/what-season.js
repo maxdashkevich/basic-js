8dcb99a786dbeeceaad750c281878ccc28cb51eb
maxim dashkevich <maximkacr7@mail.com>
2020-03-19 19:12:20 +0300

    feat: complete what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,13 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  };
+  if (!(Object.prototype.toString.call(date) == '[object Date]' )) throw new Error();
+
+  let month = date.getMonth();
+  
+  return (month >= 0 && month <= 1 || month == 11 ) ? 'winter' : 
+  (month > 1 && month <= 4) ? 'spring' :
+  (month > 4 && month <= 7) ? 'summer' :
+  (month > 7 && month <= 10) ? 'autumn' : false;
 };
