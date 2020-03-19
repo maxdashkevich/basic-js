c5cee4360f99f33fd46ed6b16f093c115c3c8461
maxim dashkevich <maximkacr7@mail.com>
2020-03-19 19:50:32 +0300

    feat: correct hanoi-tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     let turns = Math.pow(2, disksNumber) - 1;
     let speed = turns / (turnsSpeed / 3600);
-    return { turns: turns, speed: speed }
+    return ({ turns: turns, seconds: speed });
 }
