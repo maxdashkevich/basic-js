dd880eab6ef43a430349586fb3268c4a685edab6
Maksim Dashkevich <maximkacr7@gmail.com>
2021-02-21 14:24:54 +0300

    feat: add vigenere-cipher and extended-repeater tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,16 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const arr = [String(str)];
+  const addArr = [String(options.addition)];
+  for (let i = 0; i < Number(options.repeatTimes) - 1; i++) {
+    arr.push(String(str))
+  }
+
+  if (options.addition || String(options.addition) === "false" || String(options.addition) === "null") {
+    for (let i = 0; i < Number(options.additionRepeatTimes) - 1; i++) {
+      addArr.push(String(options.addition))
+    }
+    return arr.map((item) => item + addArr.join(options.additionSeparator || '|')).join(options.separator || '+');
+  }
+
+  return arr.join(options.separator || '+')
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,65 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  constructor(bool) {
+    this.bool = bool;
+  }
+  vigenerSquare() {
+    const words = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    const square = [];
+    for (var i = 0; i < words.length; i++) {
+      square[i] = words.slice(i).concat(words.slice(0, i));
     }
+    return square;
+  }
+  encrypt(string, key) {
+    if (string && key) {
+      const square = this.vigenerSquare();
+      const words = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+      let newString = string.toUpperCase();
+      let newKey = key.split('')
+      let i = newKey.length;
+      for (i = i; i < string.length; i += i) {
+        key = key.concat(key);
+      }
+      newKey = key.toUpperCase().split('').slice(0, string.length)
+      let str = "";
+      let j = 0
+      for (let i = 0; i < newString.length; i++) {
+        if (words.indexOf(newString[i]) !== -1) {
+          str += square[words.indexOf(newString[i])][words.indexOf(newKey[j])];
+          j++
+        } else str += newString[i];
+      }
+      if (this.bool !== false) {
+        return str;
+      } else return str.split('').reverse().join('');
+    } else throw Error('Error')
+  }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+  decrypt(string, key) {
+    if (string && key) {
+      const square = this.vigenerSquare();
+      const words = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+      let newString = string.toUpperCase();
+      let newKey = key.split('');
+      let i = newKey.length;
+      for (i = i; i < string.length; i += i) {
+        key = key.concat(key);
+      }
+      newKey = key.toUpperCase().split('').slice(0, string.length)
+      let str = "";
+      let j = 0
+      for (let i = 0; i < newString.length; i++) {
+        if (words.indexOf(newString[i]) !== -1) {
+          let row = words.indexOf(newKey[j])
+          let coll = square[row].indexOf(newString[i]);
+          str += words[coll];
+          j++
+        } else str += newString[i];
+      }
+      if (this.bool !== false) {
+        return str;
+      } else return str.split('').reverse().join('');
+    } else throw Error('Error')
+  }
 }
-
 module.exports = VigenereCipheringMachine;
